3819b7d8a19fe0044cde64956472514ce82a8fba
Hammad <devhammad.m@gmail.com>
2024-04-08 16:34:55 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,109 @@
-import Image from "next/image";
+"use client";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import axios from "axios";
+import { emotionConfig } from "./config";
+import { ColorRing } from "react-loader-spinner";
+import Link from "next/link";
 
-export default function Home() {
+const Home = () => {
+  const defaultColor = "#cccccc";
+  const [rows, setrows] = useState(2);
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState<{ label: string; score: number }[]>();
+  const [loading, setLoading] = useState(false);
+  const [color, setColor] = useState(defaultColor);
+  const [tagsVisible, settagsVisible] = useState(false);
+
+  useEffect(() => {
+    const inputTimeout = setTimeout(() => {
+      runPredictions();
+    }, 1000);
+
+    return () => clearTimeout(inputTimeout);
+  }, [input]);
+
+  useEffect(() => {
+    handleColor();
+    settagsVisible(true);
+  }, [output]);
+
+  function handleColor() {
+    if (output && output.length > 0) {
+      const colorKey = output[0].label;
+      const colorHex = (emotionConfig as any)[colorKey].colorHex;
+      setColor(colorHex);
+    }
+  }
+
+  async function runPredictions() {
+    if (input) {
+      setLoading(true);
+      settagsVisible(false);
+      //sending API call
+      const res = await axios.post("/api/emotion", { input });
+      setOutput(res.data.filteredResponse);
+
+      setLoading(false);
+    }
+  }
+
+  function handleInputChange(event: ChangeEvent<HTMLTextAreaElement>): void {
+    setInput(event.target.value);
+
+    // increases the number of rows if required
+    const newRows = Math.max(1, Math.ceil(event.target.scrollHeight / 20));
+    setrows(newRows);
+  }
+
+  function renderLoader() {
+    return <ColorRing
+    visible={true}
+    height="80"
+    width="80"
+    ariaLabel="color-ring-loading"
+    wrapperStyle={{}}
+    wrapperClass="color-ring-wrapper"
+    colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+    />
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main
+      style={{ background: color + "aa" }}
+      className="flex flex-col min-h-screen items-center p-24 gap-4 transition-all delay-500"
+    >
+      <h1 className="text-2xl md:text-4xl font-bold tracking-tighter">
+        🎨🖌️ Paint My Mood 🖌️🎨
+      </h1>
+      <div className="w-1/2 min-w-80 border-2 border-black p-4 rounded-xl">
+        <textarea
+          className="outline-none resize-none block w-full text-sm bg-transparent text-slate-800"
+          placeholder="type how you feel..."
+          rows={rows}
+          onChange={handleInputChange}
+        ></textarea>
+      </div>
+      <p>{"> " + input}</p>
+      <div className="flex flex-wrap items-center justify-center gap-2">
+        {output?.map(({ label, score }) => (
+          <span
+            key={label}
+            style={{ opacity: tagsVisible ? 1 : 0 }}
+            className="cursor-pointer bg-indigo-100 text-indigo-800 text-lg px-4 py-1 rounded-full border border-indigo-400 transition-all"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            {label} {(emotionConfig as any)[label].emoji}
+          </span>
+        ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {loading && renderLoader()}
+      <div className="mt-auto flex flex-col items-center">
+        <p className=" text-slate-800 font-semibold">Sentiment Analysis Project</p>
+        <h3 className="text-xs  text-slate-800">(Made by: Muhammad Hammad)</h3>
+        <br />
+        <Link href="https://www.linkedin.com/in/-hammad/" target="_blank" className="underline text-blue-900">Lets Connect</Link>
       </div>
     </main>
   );
-}
+};
+
+export default Home;
